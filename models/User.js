5499d7069ca1bbf8535e6df2501522cf8e70530d
Jamshid2204 <jamshidjumaboyev2204@gmail.com>
2024-04-24 11:06:37 +0500

    first commit

--- models/User.js
+++ models/User.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  uid:{type:String, required: true, unique: true},  
   username:{type:String, required: true, unique: true},  
   email:{type:String, required: true, unique: true},  
   password:{type:String, required: true},  
+  uid:{type:String, required: true, unique: true},  
   location:{type:String, required: false},
   phone:{type:String, required: false},
   updated:{type:Boolean, default: false},
-  isAgent:{type:Boolean, default: false},
   isAdmin:{type:Boolean, default: false},
+  isAgent:{type:Boolean, default: false},
   skills:{type:Boolean, default: false, required:false},
   profile:{type:String,required:true, default:"https://cdn.icon-icons.com/icons2/2699/PNG/512/google_logo_icon_169090.png"},
   
